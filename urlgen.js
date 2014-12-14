561723880ed22b65abf3dc94b73ed08f1e0f258c
sriramsubramanian <sriram@innervoice.in>
2014-12-14 17:13:51 +0530

    R2 commit to master

---
+++ urlgen.js
@@ -0,0 +1,167 @@
+/*
+ * Copyright (c) Innervoice Consulting 2012. All rights reserved.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * User: sriram
+ * Date: 28/10/12
+ * Time: 3:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+// method that returns value of a query parameter in the window URL
+
+function returnQueryValue(queryParam) {
+    // TODO need to refactor and decouple from window.location
+    queryParamList = window.location.search.substring(1);
+    parsedParams = queryParamList.split("&");
+    var finalParamValue = "";
+    for (i=0;i<parsedParams.length;i++) {
+        param = parsedParams[i].split("=");
+        if (param[0] == queryParam) {
+            finalParamValue = param[1];
+        }
+    }
+    return finalParamValue;
+}
+
+
+// The isUrl function is courtesy - http://dzone.com/snippets/validate-url-regexp
+
+function isUrl(s)
+{
+    var regexp = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+    return regexp.test(s);
+}
+
+function isCheckboxChecked(checkboxId)
+{
+    return $(checkboxId).is(":checked");
+}
+
+function generateUtmUrl(medium)
+{
+    var utmCampaign = $("#utm_campaign").val();
+    var utmSource = $("#utm_source").val();
+    var urlTextDiv = document.getElementById("url");
+    var finalUrlString = "";
+    if(urlTextDiv) {
+        var urlValue = urlTextDiv.value;
+        if(urlValue)
+        {
+            finalUrlString = urlValue + "/?utm_campaign=" +
+                utmCampaign + "&utm_medium=" + medium +
+                "&utm_source=" + utmSource;
+        }
+    }
+    return finalUrlString;
+}
+
+function publishValueToDiv(checkboxId, resultDiv, mediumName, linkTitle)
+{
+    var finalValue = "";
+    if(linkTitle == undefined)
+    {
+        linkTitle = mediumName;
+
+    }
+    if(isCheckboxChecked(checkboxId))
+    {
+        finalValue = generateUtmUrl(mediumName);
+    }
+    publishFinalValueToDiv(resultDiv, finalValue, linkTitle);
+}
+
+function publishFinalValueToDiv(divId, value, linkTitle)
+{
+    var resultDiv = document.getElementById(divId);
+    if(isUrl(value.toString()))
+    {
+        resultDiv.innerHTML = "<a href='"+value+"'>" + linkTitle +"</a>";
+    }
+    else
+    {
+        resultDiv.innerHTML = value;
+    }
+
+}
+
+function generateResults()
+{
+    publishValueToDiv("#facebook", "results_for_facebook", "facebook");
+
+    publishValueToDiv("#linkedin", "results_for_linkedin", "linkedin");
+
+    publishValueToDiv("#googleplus", "results_for_googleplus", "googleplus");
+
+    publishValueToDiv("#twitter", "results_for_twitter", "twitter");
+
+    publishValueToDiv("#allpurpose", "results_for_allpurpose", "allpurpose");
+
+    storeAllPreferences();
+
+}
+
+function storeAllPreferences() {
+    var prefManager = new PreferenceManager();
+
+    var facebookCheckbox = $('#facebook').is(':checked');
+    console.log("Value of Facebook Check box: "+facebookCheckbox);
+    prefManager.setFacebookSelected(facebookCheckbox);
+
+    var linkedInCheckbox = $('#linkedin').is(':checked');
+    console.log("Value of LinkedIn Check box: "+linkedInCheckbox);
+    prefManager.setLinkedInSelected(linkedInCheckbox);
+
+    var googlePlusCheckbox = $('#googleplus').is(':checked');
+    console.log("Value of google plus Check box: "+googlePlusCheckbox);
+    prefManager.setGooglePlusSelected(googlePlusCheckbox);
+
+    var twitterCheckbox = $('#twitter').is(':checked');
+    console.log("Value of twitter Check box: "+twitterCheckbox);
+    prefManager.setTwitterSelected(twitterCheckbox);
+
+    var campaignText = $('#utm_campaign').val();
+    console.log("Value of Campaign: "+campaignText);
+    prefManager.setCampaign(campaignText);
+
+    var sourceText = $('#utm_source').val();
+    console.log("Value of Source: "+sourceText);
+    prefManager.setSource(sourceText);
+
+}
+
+function loadAllPreferences() {
+    var prefManager = new PreferenceManager();
+
+    var facebookCheckbox = prefManager.isFacebookSelected();
+    console.log("Facebook value from Storage: "+facebookCheckbox);
+    $("#facebook").prop('checked', facebookCheckbox);
+
+    var linkedInCheckbox = prefManager.isLinkedInSelected();
+    console.log("LinkedIn value from Storage: "+linkedInCheckbox);
+    $("#linkedin").prop('checked', linkedInCheckbox);
+
+    var googlePlusCheckbox = prefManager.isGooglePlusSelected();
+    console.log("Google Plus value from Storage: "+googlePlusCheckbox);
+    $("#googleplus").prop("checked", googlePlusCheckbox);
+
+    var twitterCheckbox = prefManager.isTwitterSelected();
+    console.log("Twitter value from Storage: "+twitterCheckbox);
+    $("#twitter").prop("checked", twitterCheckbox);
+
+    var campaignText = prefManager.getCampaign();
+    console.log("Campaign value from Storage: "+campaignText);
+    $("#utm_campaign").val(campaignText);
+
+    var sourceText = prefManager.getSource();
+    console.log("Source value from Storage: "+sourceText);
+    $("#utm_source").val(sourceText);
+}
---
+++ utm.pref.mgr.js
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) Innervoice Consulting 2012. All rights reserved.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * User: sriram
+ * Date: 16/12/12
+ * Time: 10:02 AM
+ * This JavaScript file represents the PreferenceManager class. This class provides an OO wrapper around
+ * HTML5 localStorage for storing and loading UTM related preferences.
+ */
+
+function PreferenceManager()
+{
+    this.facebookKey = "iv.utm.facebook";
+    this.linkedInKey = "iv.utm.linkedin";
+    this.googlePlusKey = "iv.utm.googpleplus";
+    this.twitterKey = "iv.utm.twitter";
+    this.campaignKey = "iv.utm.campaign";
+    this.sourceKey = "iv.utm.source";
+}
+
+// Method that returns the value from localStorage. If the localStorage value is null, then defaultValue is returned
+
+PreferenceManager.prototype.loadPrefFromLocalStorage = function(key, defaultValue) {
+    var preferenceValue = defaultValue;
+    if(window.localStorage )
+    {
+        preferenceValue = localStorage.getObject(key);
+        if(preferenceValue == null)
+        {
+            console.log("Value NULL for key: "+key);
+            preferenceValue = defaultValue;
+        }
+        console.log("Key: "+key+":::Object type of Value: "+typeof preferenceValue+":::Value: "+preferenceValue);
+    }
+    return preferenceValue;
+}
+
+// Method that stores the key-value pair in localStorage
+
+PreferenceManager.prototype.storePrefToLocalStorage = function(key, value) {
+    if(window.localStorage)
+    {
+        localStorage.setObject(key, value);
+        console.log("Value: "+value+" stored in local storage.");
+    }
+}
+
+PreferenceManager.prototype.isFacebookSelected = function() {
+    // TODO - add boolean value type check. If not boolean return default value
+    return this.loadPrefFromLocalStorage(this.facebookKey, true);
+}                                                       ;
+
+PreferenceManager.prototype.setFacebookSelected = function(value) {
+    // TODO - add boolean value type check
+    this.storePrefToLocalStorage(this.facebookKey, value);
+}
+
+PreferenceManager.prototype.isLinkedInSelected = function() {
+    // TODO - add boolean value type check. If not boolean return default value
+    return this.loadPrefFromLocalStorage(this.linkedInKey, true);
+}
+
+PreferenceManager.prototype.setLinkedInSelected = function(value) {
+    // TODO - add boolean value type check
+    this.storePrefToLocalStorage(this.linkedInKey, value);
+}
+
+PreferenceManager.prototype.isGooglePlusSelected = function() {
+    // TODO - add boolean value type check. If not boolean return default value
+    return this.loadPrefFromLocalStorage(this.googlePlusKey, true);
+}
+
+PreferenceManager.prototype.setGooglePlusSelected = function(value) {
+    // TODO - add boolean value type check
+    this.storePrefToLocalStorage(this.googlePlusKey, value);
+}
+
+PreferenceManager.prototype.isTwitterSelected = function() {
+    // TODO - add boolean value type check. If not boolean return default value
+    return this.loadPrefFromLocalStorage(this.twitterKey, true);
+}
+
+PreferenceManager.prototype.setTwitterSelected = function(value) {
+    // TODO - add boolean value type check
+    this.storePrefToLocalStorage(this.twitterKey, value);
+}
+
+PreferenceManager.prototype.getCampaign = function() {
+    return this.loadPrefFromLocalStorage(this.campaignKey, "promote_blog");
+}
+
+PreferenceManager.prototype.setCampaign = function(value) {
+    this.storePrefToLocalStorage(this.campaignKey, value);
+}
+
+PreferenceManager.prototype.getSource = function() {
+    return this.loadPrefFromLocalStorage(this.sourceKey, "innervoice.in");
+}
+
+PreferenceManager.prototype.setSource = function(value) {
+    this.storePrefToLocalStorage(this.sourceKey, value);
+}
